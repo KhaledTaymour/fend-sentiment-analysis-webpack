b51d50a5bef6cb4ed6b4839986c9f5f74381688a
K_Taymour <khaled.taymour@esrinea.com>
2021-03-22 06:41:40 +0200

    [feat]: changes in webpack config files: Both webpack.prod.js & webpack.dev.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,43 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
-    entry: './src/client/index.js',
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
-    ]
-}
+  entry: "./src/client/index.js",
+  mode: "production",
+  devtool: "source-map",
+  stats: "minimal",
+  output: {
+    libraryTarget: "var",
+    library: "client",
+  },
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: "/node_modules/",
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+    new CleanWebpackPlugin({
+      dry: true,
+      verbose: true,
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
+    }),
+    // TODO: configure workbox-webpack-plugin
+    new WorkboxPlugin.GenerateSW(),
+  ],
+};
