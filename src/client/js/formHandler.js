20eb13140f132082fea37d0f14f60841897eb367
K_Taymour <khaled.taymour@esrinea.com>
2021-03-22 21:08:20 +0200

    [feat]: FE: handling submit

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,43 @@
-function handleSubmit(event) {
-    event.preventDefault()
+const post = async (url = "", data = {}) => {
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  try {
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+export const handleSubmit = async () => {
+  console.log("inside handle submit");
+  const url = document.querySelector(".url-input").value;
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
-}
-
-export { handleSubmit }
+  if (client.validateURL(url)) {
+    await post("http://localhost:8081/analyze-url", { url }).then((data) => {
+      console.log("then in formhandler", data);
+      document.querySelector(".analysis-results__agreement-value").textContent =
+        data.agreement;
+      document.querySelector(
+        ".analysis-results__confidence-value"
+      ).textContent = data.confidence;
+      document.querySelector(".analysis-results__irony-value").textContent =
+        data.irony;
+      document.querySelector(".analysis-results__score_tag-value").textContent =
+        data.score_tag;
+      document.querySelector(
+        ".analysis-results__subjectivity-value"
+      ).textContent = data.subjectivity;
+      document.querySelector(".analysis-results__text-value").textContent =
+        data.text;
+    });
+  } else {
+    alert("URL invalid");
+  }
+};
